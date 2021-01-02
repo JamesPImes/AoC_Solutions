c67e477c0b2fa7f5d02f2a472d10a3aa0d8a2eb0
James P. Imes <jamesimes@gmail.com>
2021-01-01 16:03:09 -0800

    2020, Day 22, Part 2 solution

--- 2020/aoc2020_22.py
+++ 2020/aoc2020_22.py
@@ -13,7 +13,7 @@ def parse_raw(raw):
     return starting_hands
 
 
-def new_turn(hands):
+def new_turn_part1(hands):
     p1, p2 = hands[1].pop(0), hands[2].pop(0)
     assert p1 != p2
     if p1 > p2:
@@ -30,14 +30,73 @@ def score_hand(hand):
     return score
 
 
+def new_game_part2(hands):
+    winner = None
+    card_orders = set()
+    while winner is None:
+        winner = new_round_part2(hands, card_orders)
+    return winner
+
+
+def new_round_part2(hands, card_orders):
+
+    # Check if the cards have been in this order before. If so, player 1
+    # wins the GAME.
+    t1, t2 = tuple(hands[1]), tuple(hands[2])
+    if (t1, t2) in card_orders:
+        return 1
+
+    # Keep track of this new order of cards.
+    card_orders.add((t1, t2))
+
+    # Draw top card
+    p1, p2 = hands[1].pop(0), hands[2].pop(0)
+
+    # Evaluate who won the round
+    if len(hands[1]) >= p1 and len(hands[2]) >= p2:
+        round_winner = recursive_combat(hands, p1, p2)
+    elif p1 > p2:
+        round_winner = 1
+    else:
+        round_winner = 2
+
+    # Put the two cards back in the round winner's deck
+    if round_winner == 1:
+        hands[1].append(p1)
+        hands[1].append(p2)
+    else:
+        hands[2].append(p2)
+        hands[2].append(p1)
+
+    # Check for a winner of the GAME (not the round).
+    if len(hands[1]) == 0:
+        return 2
+    elif len(hands[2]) == 0:
+        return 1
+    else:
+        return None
+
+
+def recursive_combat(hands, p1, p2):
+    # Copy the relevant portion of the hands.
+    hands = {1: hands[1][:p1], 2: hands[2][:p2]}
+
+    # Start a new game with those hands and return the winner.
+    return new_game_part2(hands)
+
+
 def part1(raw):
     hands = parse_raw(raw)
-    total_cards = len(hands[1]) + len(hands[2])
 
-    while total_cards not in [len(hands[1]), len(hands[2])]:
-        new_turn(hands)
+    while 0 not in [len(hands[1]), len(hands[2])]:
+        new_turn_part1(hands)
     return max([score_hand(hands[1]), score_hand(hands[2])])
 
+
+def part2(raw):
+    hands = parse_raw(raw)
+    new_game_part2(hands)
+    return max([score_hand(hands[1]), score_hand(hands[2])])
 
 puz_in = aoctools.puzzle_input(2020, 22)
 test_data = '''Player 1:
@@ -55,7 +114,7 @@ Player 2:
 10'''
 
 aoctools.test(1, part1, test_data, 306)
-# aoctools.test(2, part2, test_data, 0)
+aoctools.test(2, part2, test_data, 291)
 
 aoctools.submit(1, part1, puz_in)
-# aoctools.submit(2, part2, puz_in)
+aoctools.submit(2, part2, puz_in)
